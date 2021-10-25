66a40f39f7760926189af491144d4f6185ca5273
Dawid Iwaniuk <dawuso@gmail.com>
2021-10-25 13:05:58 +0200

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+^
+.app/src/main/java/pl/edu/pb/quiz/Question.java,9\b\9bc3c4899182c1c535aa1ef2de62199533dcc47b
---
+++ app/src/main/java/pl/edu/pb/quiz/Question.java
@@ -0,0 +1,16 @@
+package pl.edu.pb.quiz;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Question {
+    private int questionId;
+    private boolean trueAnswer;
+
+    public Question(int questionId, boolean trueAnswer){
+        this.questionId = questionId;
+        this.trueAnswer = trueAnswer;
+    }
+}
